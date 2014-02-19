5ed3d412797ce288723e00a16982bf325d965bbd
unknown <gordon.k@.corporate.local>
2014-02-19 13:59:07 -0700

    2014-02-19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ HaveTodo::Application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'kwokgordon.android@gmail.com',
-  :password             => '',
+  :user_name            => 'havetodoapp@gmail.com',
+  :password             => 'havetodoapp123',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
 

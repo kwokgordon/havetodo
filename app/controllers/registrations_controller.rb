6e368a85df2523a2de0d18d27f541a1dca57815f
unknown <gordon.k@.corporate.local>
2014-02-19 11:54:08 -0700

    2014-02-19

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
-#  prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
-#  prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
+  prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
   prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
     
   protect_from_forgery with: :exception
@@ -13,6 +13,10 @@ class RegistrationsController < Devise::RegistrationsController
   end
   
   def create
+    super do |resource|
+      BackgroundWorker.trigger(resource)
+    end
+=begin    
     build_resource(sign_up_params)
     resource.skip_confirmation!
 
@@ -32,7 +36,7 @@ class RegistrationsController < Devise::RegistrationsController
       respond_with resource
     end
 
-=begin    
+
     if resource.save
       sign_in resource
       render :text => "Created Registration"
@@ -59,6 +63,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def edit
+    super do |resource|
+      BackgroundWorker.trigger(resource)
+    end
+  end
+
+  def destroy
     super do |resource|
       BackgroundWorker.trigger(resource)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :token_authenticatable
-
-  def skip_confirmation!
-    self.confirmed_at = Time.now
-  end
+         :token_authenticatable
 
 end

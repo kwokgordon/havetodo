109c4fe0cc3fc42ba3abf60f66beb400895933c9
unknown <gordon.k@.corporate.local>
2014-02-18 17:09:25 -0700

    2014-02-18

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,7 +30,7 @@ class SessionsController < Devise::SessionsController
   end
 =end
 
-  protect
+  protected
   
     def after_sign_in_path_for(resource)
       render :text => "after_sign_in_path_for"

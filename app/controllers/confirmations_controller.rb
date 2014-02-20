46af6944e1b7a9b8daa05211991517945dfb4677
unknown <gordon.k@.corporate.local>
2014-02-20 13:42:30 -0700

    2014-02-20

--- app/controllers/confirmations_controller.rb
+++
@@ -1,9 +0,0 @@
-class ConfirmationsController < Devise::ConfirmationsController
-  
-  protected
-  
-  def after_resending_confirmation_instructions_path_for(resource_name)
-    new_user_session_path
-  end
-  
-end

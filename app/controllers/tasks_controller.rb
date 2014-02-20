2ac02f45d588702552c7beacb2d17b9f722578b8
unknown <gordon.k@.corporate.local>
2014-02-20 11:41:57 -0700

    2014-02-20

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,8 +38,7 @@ class TasksController < ApplicationController
       if @task.save
         @user.tasks << @task
         
-        format.html { redirect_to user_tasks_url(@user), notice: 'Task was successfully created.' }
-#        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render action: 'show', status: :created, location: @task }
       else
         format.html { render action: 'new' }
@@ -53,8 +52,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to user_tasks_url(@user), notice: 'Task was successfully updated.' }
-#        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -68,8 +66,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to user_tasks_path(@user) }
-#      format.html { redirect_to tasks_url }
+      format.html { redirect_to tasks_url }
       format.json { head :no_content }
     end
   end

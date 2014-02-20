86857ce0e537888fa984fca437794e04b5203aa4
unknown <gordon.k@.corporate.local>
2014-02-20 11:22:49 -0700

    2014-02-20

--- db/migrate/20140219235343_create_users_tasks.rb
+++ db/migrate/20140219235343_create_users_tasks.rb
@@ -1,10 +1,10 @@
 class CreateUsersTasks < ActiveRecord::Migration
   def change
-    create_table :users_tasks, :id => false do |t|
-      t.integer :user_id, :null => false
+    create_table :tasks_users, :id => false do |t|
       t.integer :task_id, :null => false
+      t.integer :user_id, :null => false
     end
     
-    add_index :users_tasks, [:user_id, :task_id], :unique => true
+    add_index :tasks_users, [:user_id, :task_id], :unique => true
   end
 end

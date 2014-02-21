322eac0ce4cf0d6f94e9717a90436aee83d3f9d2
unknown <gordon.k@.corporate.local>
2014-02-20 17:33:58 -0700

    2014-02-20

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      redirect_to tasks_path
+    else
+      respond_to do |format|
+        format.html 
+      end
+    end
   end
   
   def blank

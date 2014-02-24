b10e86c4b620371310f8400e4d5497ad4f750954
unknown <Gordon@.(none)>
2014-02-24 01:12:27 -0700

    20140223

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,6 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function highlightTask(x) {
+	x.style.background = #C0C0C0;
+}
+
+function unhighlightTask(x) {
+	x.style.background = #FFFFFF;
+}
+
 function toggleComplete(id) {
 	document.getElementById(id).innerHTML = "hello";
 }

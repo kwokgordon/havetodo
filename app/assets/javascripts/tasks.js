d3cd661b5248dacd82f2611ffae2e2ea8d8eb679
unknown <Gordon@.(none)>
2014-02-24 01:06:10 -0700

    20140223

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,3 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function toggleComplete(id) {
+	document.getElementById(id).innerHTML = "hello";
+}

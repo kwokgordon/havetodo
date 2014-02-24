c698ba1a3819f57e28a270b79cd5737c59f6fe64
unknown <gordon.k@.corporate.local>
2014-02-24 11:27:54 -0700

    20140223

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require bootstrap-datetimepicker
-//= require locales/bootstrap-datetimepicker.<locale>
 //= require turbolinks
 //= require_tree .

204f547bd555ff2235402507d2895aade12ef7ed
unknown <gordon.k@.corporate.local>
2014-02-25 12:31:02 -0700

    20140225

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,37 +16,6 @@ class RegistrationsController < Devise::RegistrationsController
     super do |resource|
       BackgroundWorker.trigger(resource)
     end
-=begin    
-    build_resource(sign_up_params)
-    resource.skip_confirmation!
-
-    if resource.save
-      yield resource if block_given?
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
-    end
-
-
-    if resource.save
-      sign_in resource
-      render :text => "Created Registration"
-#      redirect_to @user
-    else
-#      render :text => "Failure Registration"
-      render :new
-    end
-
-=end
   end
 
 

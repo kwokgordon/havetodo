44193b05ebe067afe6c3db62aae7ade4c9962398
unknown <gordon.k@.corporate.local>
2014-02-24 17:06:03 -0700

    20140223

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -9,7 +9,7 @@ class Api::RegistrationsController < Devise::RegistrationsController
 
   def create
     build_resource(params[:user])
-#    resource.skip_confirmation!
+    resource.skip_confirmation!
     
     if resource.save
       sign_in resource

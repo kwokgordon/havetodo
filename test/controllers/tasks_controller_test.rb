648e544a7eec0eea116623c1e97ffc3f4cdb4aff
unknown <gordon.k@.corporate.local>
2014-02-26 10:49:44 -0700

    20140226

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,37 +6,36 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index, :user_id => users(:giles).id
+    get :index
     assert_response :success
     assert_not_nil assigns(:tasks)
   end
 
   test "should get new" do
-    get :new, :user_id => users(:giles).id
+    get :new
     assert_response :success
   end
 
   test "should create task" do
     assert_difference('Task.count') do
-      post :create, task: { completed: @task.completed, completed_date: @task.completed_date, completed_user_id: @task.completed_user_id, due_date: @task.due_date, due_time: @task.due_time, name: @task.name, note: @task.note },
-      :user_id => users(:giles).id
+      post :create, task: { completed: @task.completed, completed_date: @task.completed_date, completed_user_id: @task.completed_user_id, due_date: @task.due_date, due_time: @task.due_time, name: @task.name, note: @task.note }
     end
 
     assert_redirected_to task_path(assigns(:task))
   end
 
   test "should show task" do
-    get :show, id: @task, :user_id => users(:giles).id
+    get :show, id: @task
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @task, :user_id => users(:giles).id
+    get :edit, id: @task
     assert_response :success
   end
 
   test "should update task" do
-    patch :update, id: @task, task: { completed: @task.completed, completed_date: @task.completed_date, due_date: @task.due_date, name: @task.name, note: @task.note }
+    patch :update, id: @task, task: { completed: @task.completed, completed_date: @task.completed_date, completed_user_id: @task.completed_user_id, due_date: @task.due_date, due_time: @task.due_time, name: @task.name, note: @task.note }
     assert_redirected_to task_path(assigns(:task))
   end
 

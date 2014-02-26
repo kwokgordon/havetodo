5f0beca1f4160a6357d904ca3a46e5d2c944d288
unknown <gordon.k@.corporate.local>
2014-02-26 10:41:03 -0700

    20140226

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,31 +6,32 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, :user_id => users(:giles).id
     assert_response :success
     assert_not_nil assigns(:tasks)
   end
 
   test "should get new" do
-    get :new
+    get :new, :user_id => users(:giles).id
     assert_response :success
   end
 
   test "should create task" do
     assert_difference('Task.count') do
-      post :create, task: { completed: @task.completed, completed_date: @task.completed_date, due_date: @task.due_date, name: @task.name, note: @task.note }
+      post :create, task: { completed: @task.completed, completed_date: @task.completed_date, completed_user_id: @task.completed_user_id, due_date: @task.due_date, due_time: @task.due_time, name: @task.name, note: @task.note },
+      :user_id => users(:giles).id
     end
 
     assert_redirected_to task_path(assigns(:task))
   end
 
   test "should show task" do
-    get :show, id: @task
+    get :show, id: @task, :user_id => users(:giles).id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @task
+    get :edit, id: @task, :user_id => users(:giles).id
     assert_response :success
   end
 

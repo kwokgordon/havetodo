0df49112378b37bb2e273c46ae86ebee7f31bafe
unknown <gordon.k@.corporate.local>
2014-02-26 12:10:40 -0700

    20140226

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -34,18 +34,19 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit, id: @task
+    get :edit, id: @task, :user_id => users(:one).id
     assert_response :success
   end
 
   test "should update task" do
-    patch :update, id: @task, task: { completed: @task.completed, completed_date: @task.completed_date, completed_user_id: @task.completed_user_id, due_date: @task.due_date, due_time: @task.due_time, name: @task.name, note: @task.note }
+    patch :update, id: @task, task: { completed: @task.completed, completed_date: @task.completed_date, completed_user_id: @task.completed_user_id, due_date: @task.due_date, due_time: @task.due_time, name: @task.name, note: @task.note },
+    :user_id => users(:one).id
     assert_redirected_to task_path(assigns(:task))
   end
 
   test "should destroy task" do
     assert_difference('Task.count', -1) do
-      delete :destroy, id: @task
+      delete :destroy, id: @task, :user_id => users(:one).id
     end
 
     assert_redirected_to tasks_path

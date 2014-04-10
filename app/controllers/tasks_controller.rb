5920840b0e5f4e37ce24a88675e2f8eb3e0b82ea
unknown <gordon.k@.corporate.local>
2014-04-09 18:22:07 -0600

    20140409

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,7 @@ class TasksController < ApplicationController
   # GET /tasks/1
   # GET /tasks/1.json
   def show
+    share_task
   end
 
   # GET /tasks/new
@@ -41,6 +42,7 @@ class TasksController < ApplicationController
 
   # GET /tasks/1/edit
   def edit
+    share_task
   end
 
   # POST /tasks
@@ -123,6 +125,14 @@ class TasksController < ApplicationController
     end
   end
   
+  def addFriend
+    
+  end
+  
+  def removeFriend
+    
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def get_user
@@ -146,6 +156,13 @@ class TasksController < ApplicationController
           cookies[:due_time] = { value: @task.due_time.strftime("%H:%M:%S.%L"), path: '/users' }
         end
       end
+    end
+    
+    def share_task
+      @friends = @user.friendships.accepted.pluck(:friend_id)
+      @friends = @friends + @user.inverse_friendships.accepted.pluck(:user_id)
+    
+      @task_shared = @task.users.pluck(:id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ HaveTodo::Application.routes.draw do
 
   resources :tasks, path: '/users/tasks' do
     post '/toggleComplete' => 'tasks#toggleComplete'
+    post '/addFriend' => 'tasks#addFriend'
+    delete '/removeFriend' => 'tasks#removeFriend'
   end
   
   resources :friendships, path: '/users/friendships' do

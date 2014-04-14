1560b6a229f420ff4a736798011f9e298fc8b4b9
unknown <gordon.k@.corporate.local>
2014-04-14 15:49:37 -0600

    20140414

--- db/migrate/20140414194705_create_users_tasklists.rb
+++ db/migrate/20140414194705_create_users_tasklists.rb
@@ -1,4 +1,4 @@
-class CreateTasklistsUsers < ActiveRecord::Migration
+class CreateUsersTasklists < ActiveRecord::Migration
   def change
     create_table :tasklists_users, :id => false do |t|
       t.integer :user_id, :null => false

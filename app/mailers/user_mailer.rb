f8bcb3f969076164ee721b0c40e3ce279c48942f
Gordon <kwokgordon@gmail.com>
2014-04-20 02:07:19 -0600

    20140420

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,13 @@ class UserMailer < ActionMailer::Base
   
   def invite_friend(user, friend_email)
     @user = user
+    if Rails.env.development?
+      @url = "http://184.64.50.5:3000/users/sign_up"
+    elsif Rails.env.production?
+      @url = "http://fathomless-island-6793.herokuapp.com/users/sign_up"
+    else
+      @url = ""
+    end
     
     mail(to: friend_email, subject: "Someone invited you to HaveTodo!")
   end

c3158ab5819d8872715863162ac177ecba9467f9
Gordon <kwokgordon@gmail.com>
2014-04-20 18:28:21 -0600

    20140420

--- db/migrate/20140414194534_create_tasklists.rb
+++ db/migrate/20140414194534_create_tasklists.rb
@@ -2,7 +2,8 @@ class CreateTasklists < ActiveRecord::Migration
   def change
     create_table :tasklists do |t|
       t.string :name
-
+      t.string :color, :null => false, :default => "#000000"
+      
       t.timestamps
     end
   end

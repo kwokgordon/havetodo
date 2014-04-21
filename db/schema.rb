28be6778afab4a8a8eced3a8432aafc6b91acf3a
Gordon <kwokgordon@gmail.com>
2014-04-21 13:22:51 -0600

    20140421

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140414194748) do
 
   create_table "tasklists", force: true do |t|
     t.string   "name"
+    t.string   "color",      default: "#000000", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end

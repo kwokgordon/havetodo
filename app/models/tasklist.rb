a2e9fc0205bd97d16c3f97e7d4d1aedd082add8b
unknown <gordon.k@.corporate.local>
2014-04-21 18:29:39 -0600

    20140421

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -2,9 +2,10 @@ class Tasklist < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :tasks
   
-  attr_accessible :name
+  attr_accessible :name, :color
 
   validates :name, presence: true
+  validates :color, presence: true
 
 =begin
   def to_param
